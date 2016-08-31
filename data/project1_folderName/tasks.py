dd580d9da359b2563694622ad90574aa881da083
NFOURRIER <nfourrier@gmail.com>
2016-08-31 17:13:32 +1200

    add project 1 first commit

---
+++ data/project1_folderName/tasks.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+import datetime as dt
+from os import getenv
+import sys
+from collections import defaultdict
+from celery.task import task
+import celery
+
+EXCELpath = os.path.join(os.getcwd(),'data', 'output')
+
+@celery.task(name='data.project1_folderName.tasks.request')
+def request(x,*arg):
+    utc = dt.datetime.utcnow()
+    return utc
+
+@celery.task(name='data.project1_folderName.tasks.update')
+def update(start_date,end_date,*arg):
+    utc = dt.datetime.utcnow()
+    return utc
+
+@celery.task(name='data.project1_folderName.tasks.download')
+def download(start_date,end_date,*arg):
+    utc = dt.datetime.utcnow()
+    # generate some excel file
+    filename = 'filename.xlsx'
+    return filename
+
+if __name__ == "__main__":
+    request()
