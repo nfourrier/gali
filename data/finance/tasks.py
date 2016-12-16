66cd7982bbc5fc01b413679a010b537091662fb3
NFOURRIER <nfourrier@gmail.com>
2016-12-16 14:52:29 +1300

    add finance project

---
+++ data/finance/tasks.py
@@ -0,0 +1,9 @@
+from pandas_datareader import data as web
+import datetime as dt
+utc = dt.datetime.utcnow()
+start = dt.datetime(2010, 1, 1)
+end = utc
+f=web.DataReader("F", 'yahoo', start, end)
+
+
+print(f)
