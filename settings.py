ebf5316a9e3326e93aeac3052b184483e787e8a5
NFOURRIER <nfourrier@gmail.com>
2016-12-20 15:09:35 +1300

    add redis server

--- settings.py
+++ settings.py
@@ -5,6 +5,14 @@ SECRET_KEY = 'not_a_secret'
 CELERY_BROKER_URL = 'amqp://guest@localhost//'
 CELERY_RESULT_BACKEND = 'amqp://guest@localhost//'
 
+# to get it work:
+# >> pip install -U "celery[redis]"
+# start the server
+# >> redis-server
+# CELERY_BROKER_URL='redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+
+
 CELERY_RESULT_PERSISTENT = True
 
 CELERYBEAT_SCHEDULE = {
